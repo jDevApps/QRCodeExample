1ca0a1ebc097f269c08eecfa6e4d31b523bea2b8
Developer <Developer@RAPTOR>
2016-10-28 04:09:27 -0500

    Add project files.

---
+++ QRCode.WebApp/Index.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="QRCode.WebApp.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox runat="server" ID="textCode" />
+            <asp:Button Text="Generate" ID="buttonGenerate" runat="server" OnClick="buttonGenerate_Click" />
+            <hr />
+        </div>
+        <div>
+            <asp:Image runat="server" ID="imgQR" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ QRCode.WebApp/Index.aspx.cs
@@ -0,0 +1,30 @@
+﻿using MessagingToolkit.QRCode.Codec;
+using System;
+using System.Drawing;
+using System.IO;
+
+namespace QRCode.WebApp
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void buttonGenerate_Click(object sender, EventArgs e)
+        {
+            QRCodeEncoder encoder = new QRCodeEncoder();
+            Bitmap img = encoder.Encode(textCode.Text);
+            System.Drawing.Image QR = (System.Drawing.Image)img;
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                QR.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                byte[] imageBytes = ms.ToArray();
+                imgQR.ImageUrl = $"data:image/gif;base64, {Convert.ToBase64String(imageBytes)}";
+                imgQR.Height = 200;
+                imgQR.Width = 200;
+            }
+        }
+    }
+}
